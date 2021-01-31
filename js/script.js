6241fa995c80006a2e8852c564d6d66093898a7b
AlexeyProjects <62065851+AlexeyProjects@users.noreply.github.com>
2021-01-31 22:50:21 +0400

    фывфыв
    фывфыв

--- js/script.js
+++ js/script.js
@@ -1,45 +1,62 @@
-inputDefault = document.getElementsByClassName('inputDefault');
-    console.log(inputDefault);
-    //первое поле
-
-
 function focusInput(el) {
 	console.log(el);
-	el.parentElement.classList.add('focusInput');
-	let closeIcon = el.parentElement.querySelector('.fa-times');
+	el.parentElement.classList.add('fillInput');
 	el.addEventListener('blur',() => {
-	 		el.parentElement.classList.remove('focusInput');
-		});
-
-	if (el.className == 'inputDefault') {
-		el.parentElement.classList.add('focusInput');
-	}
-
-	else if (el.className == 'inputLabel') {
-		el.parentElement.classList.add('labelResize');
-
-		el.addEventListener('blur',() => {
-	 		if (el.value == 0)  {
-	 			el.parentElement.classList.remove('labelResize');
-	 			el.parentElement.classList.remove('errorInput');
-	 			closeIcon.style.display = 'none';
+	 		if (el.value.length > 0) {
+	 			return
 	 		}
 	 		else {
-	 			
+	 			el.parentElement.classList.remove('fillInput');
 	 		}
-	 		if (el.value.length >= 2) {
-	 			el.parentElement.classList.remove('errorInput');
-	 			closeIcon.style.display = 'none';
-	 		}
-	 		else if (el.value.length > 0 && el.value.length <= 1) {
-	 			el.parentElement.classList.add('errorInput');
-	 			console.log(el.parentElement.closeIcon);
-	 			closeIcon.style.display = 'block';
-	 			el.parentElement.classList.add('focusInput');
-
-	 		}
-
 		});
+}
+
+// Тоглл чек-бокса
+function checkbox(el) {
+	if (el.classList.contains('dissable')) {
+		return
 	}
-	
+	else if (el.classList.contains('checked')) {
+		console.log(123)
+		el.classList.remove('checked');
+	}
+	else {
+		el.classList.add('checked');
+	}
+}
+
+function checkbox(el) {
+	if (el.classList.contains('dissable')) {
+		return
+	}
+	else if (el.classList.contains('checked')) {
+		console.log(123)
+		el.classList.remove('checked');
+	}
+	else {
+		el.classList.add('checked');
+	}
+}
+
+function toggle(el) {
+	if (el.classList.contains('dissable')) {
+		return
+	}
+	else if (el.classList.contains('checked')) {
+		el.classList.remove('checked');
+	}
+	else {
+		el.classList.add('checked');
+	}
+}
+function radio(el) {
+	if (el.classList.contains('dissable')) {
+		return
+	}
+	else if (el.classList.contains('checked')) {
+		el.classList.remove('checked');
+	}
+	else {
+		el.classList.add('checked');
+	}
 }
